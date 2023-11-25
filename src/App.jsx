7dc79fcd163d64a6ab1c1c2d4b869322d81bb73e
alisoneoz <alisonestephanyoz@gmail.com>
2023-11-24 22:59:27 -0400

    add tailwind style to test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@
 
 const App = () => {
   return (
-    <div className='bg-black '>App</div>
+    <div className='bg-black text-red-300'>
+        <h1>Hello Ali, what's up? LFG</h1>
+        </div>
   )
 }
 
