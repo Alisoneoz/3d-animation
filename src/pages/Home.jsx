82fb05058dadb2107d9b6cd2d6370aa5af748ae8
alisoneoz <alisonestephanyoz@gmail.com>
2023-11-26 16:42:24 -0400

    create loader component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import { Canvas } from "@react-three/fiber"
-import { Loader } from 'three'
+import Loader from '../components/Loader'
+
 
 {/*       <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         POPUP
@@ -14,7 +15,7 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000, }}
       >
         <Suspense fallback={<Loader />}>
-          
+
         </Suspense>
       </Canvas>
     </section>
