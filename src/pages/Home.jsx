a985266c756ba9acfd3a54cb621c9e03f65c1000
alisoneoz <alisonestephanyoz@gmail.com>
2023-11-27 23:11:13 -0400

    add light

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
-import { Suspense } from 'react'
-import { Canvas } from "@react-three/fiber"
-import Loader from '../components/Loader'
+import { Suspense } from 'react';
+import { Canvas } from "@react-three/fiber";
+import Loader from '../components/Loader';
 
+import Island  from '../models/Island'
 
 {/*       <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         POPUP
@@ -15,7 +16,13 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000, }}
       >
         <Suspense fallback={<Loader />}>
+<directionalLight />
+<ambientLight />
+<pointLight />
+<spotLight />
+<hemisphereLight />
 
+<Island />
         </Suspense>
       </Canvas>
     </section>
